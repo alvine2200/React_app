4c6ca862d40a43f726ca6bcffe6995896a40918f
alvine2200 <alvinellavu@gmail.com>
2022-09-21 12:03:20 +0300

    layout todo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Todo from './components/Todo';
+
+function App(){
+    return (
+        <div>
+            <h1 className="page-title">My Todos</h1>
+            <Todo/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+function Todo() {
+  return (
+  <div className="card">
+      <h2 className="title">Title</h2>
+      <div className="button">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+  
+}
+export default Todo;
